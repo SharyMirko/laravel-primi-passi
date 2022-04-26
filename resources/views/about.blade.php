72aa0ba4b4dc8a6b72485dbb6959ee2ae90e2d4d
Mirko Moschella <shary.mirko@gmail.com>
2022-04-26 15:34:42 +0200

    first commit

---
+++ resources/views/about.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<header>
+        <nav>
+            <ul>
+                @foreach ($menu as $link)
+                <li><a href="{{ $link['link'] }}">{{ $link['name'] }}</a></li>
+                @endforeach
+            </ul>
+        </nav>
+    </header>
+    <main>
+        <h1>E da me che voi, io te posso cantà na canzone?</h1>
+    </main>
+</body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,59 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    $parametri = [
+        'menu' => [
+        [
+            'name' => 'Saluto',
+            'link' => '/saluto'
+        ],
+        [
+            'name' => 'About',
+            'link' => '/about'
+        ]
+        ]];
+    return view('home', $parametri);
+});
+
+Route::get('/about', function () {
+    $parametri = [
+        'menu' => [
+        [
+            'name' => 'Home',
+            'link' => '/'
+        ],
+        [
+            'name' => 'Saluto',
+            'link' => '/saluto'
+        ]
+        ]];
+    return view('about', $parametri);
+});
+
+Route::get('/saluto', function () {
+    $parametri = [
+        'menu' => [
+        [
+            'name' => 'Home',
+            'link' => '/'
+        ],
+        [
+            'name' => 'About',
+            'link' => '/about'
+        ]
+        ]];
+    return view('saluto', $parametri);
+});
